afbced55d8eb7665f816fdcbea7d6accd0d5b85e
James Pang <ling.tien5846@gmail.com>
2014-08-17 17:45:36 +0800

    Done

--- jamespang-weather-app/ViewController.swift
+++ jamespang-weather-app/ViewController.swift
@@ -8,23 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController, NSURLConnectionDataDelegate {
+class ViewController: UIViewController, NSURLConnectionDataDelegate, UITextFieldDelegate {
     
     var api: String = ""
     
     var data: NSMutableData = NSMutableData()
     
+    var searchCity: String? = ""
     
     @IBOutlet var city: UILabel!
     @IBOutlet var icon: UIImageView!
     @IBOutlet var temperature: UILabel!
+    @IBOutlet var weatherImg: UIImageView!
+    
+    @IBOutlet var searchBtn: UIButton?
+    @IBOutlet var searchBtnImg: UIImage?
+    @IBOutlet var editCity: UITextField?
+    
+    @IBOutlet var showCityName: UILabel!
+    @IBOutlet var showTemperature: UILabel!
+    @IBOutlet var goBtn: UIButton?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        self.city.text = "Taipei"
-        self.icon.image = UIImage(named: "rain_0.jpg")
         
+        println("viewDidLoad")
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.editCity!.hidden = true
+        
+        self.searchBtnImg = UIImage(named: "search1.jpg")
+        self.searchBtn!.setImage(searchBtnImg, forState: UIControlState.Normal)
+        self.goBtn!.addTarget(self, action: "onClick2:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.searchBtn!.addTarget(self, action: "onClick1:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.editCity!.addTarget(self, action: "textFieldDidBeginEditing:", forControlEvents: UIControlEvents.EditingDidBegin)
+        self.editCity!.addTarget(self, action: "textFieldDidEndEditing:", forControlEvents: UIControlEvents.EditingDidEnd)
+        
+        //self.city.text = "Taipei"
+        
+        
+        
+        //startConnection()
+    }
+    
+    func onClick1(sender: UIButton!) {
+        self.editCity!.hidden = false
+    }
+    
+    func onClick2(sender: UIButton!) {
+        println("my city: " + self.editCity!.text)
         startConnection()
     }
 
@@ -33,12 +68,35 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
         // Dispose of any resources that can be recreated.
     }
 
+    // 1. Delegation pattern
+    // 2. NSURLConnection.sendAsynchronousRequest
+    // 3. FP
     func startConnection() {
-        var restAPI: String = "http://api.openweathermap.org/data/2.5/weather?q=Taipei"
+        println("startConnection")
+        var city: String = self.editCity!.text
+        println(city)
+        var restAPI: String = "http://api.openweathermap.org/data/2.5/weather?q=\(city)"
+        println(restAPI)
         var url: NSURL = NSURL(string: restAPI)
         var request: NSURLRequest = NSURLRequest(URL: url)
         var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)
         
+    }
+    
+    
+    // 輸入中
+    func textFieldDidBeginEditing(textField: UITextField!) {
+        println("Editing...")
+        self.searchCity = textField.text
+        println(self.searchCity)
+    }
+    
+    // 輸入完成
+    func textFieldDidEndEditing(textField: UITextField!) {
+        println("Edited")
+        self.searchCity = textField.text
+        println(self.searchCity)
+        //startConnection()
     }
     
     // 下載中
@@ -52,14 +110,23 @@ class ViewController: UIViewController, NSURLConnectionDataDelegate {
         let jsonDictionary = NSJSONSerialization.JSONObjectWithData(self.data , options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary
         
         // Reading weather info
-        let temp: AnyObject? = jsonDictionary["main"]?["temp"]
+        let temp = jsonDictionary["main"]?["temp"]
+        
+        // Reading weather id
+        let weather = jsonDictionary["weather"]? as NSArray
+        if weather != nil/*let weather = jsonDictionary["weather"]? as? NSArray*/ {
+            let weatherDictionary = (weather[0] as NSDictionary)
+            let weatherID = weatherDictionary["id"] as Int
+            println("Weather ID: \(weatherID)")
+        }
         
         // Data processing
         let weatherTempCelsius = Int(round((temp!.floatValue - 273.15)))
         let weatherFahrenheit = Int(round(((temp!.floatValue - 273.15) * 1.8 ) + 32 ))
         
         // Output to UI
-        self.temperature.text = "\(weatherTempCelsius)"
+        self.showCityName.text = self.editCity!.text
+        self.showTemperature.text = "\(weatherTempCelsius) "
 
     }
     // 下載完成
